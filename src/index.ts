f2a609382b9d9f4157a084b149c4df902ac04638
Lucas Rodrigues <lucasrodrigues119816@gmail.com>
2024-10-15 15:36:36 -0300

    configuração ambiente

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+require('dotenv').config();
+import 'express-async-errors';
+import express from 'express';
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`funcionando na porta: ${port}`);
+});
