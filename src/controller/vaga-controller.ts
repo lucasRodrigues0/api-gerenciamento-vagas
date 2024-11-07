55d03bfc37707aac53b26357d6a40d5e216c2ddc
Lucas Rodrigues <lucasrodrigues119816@gmail.com>
2024-11-07 13:53:54 -0300

    refatoração de resposta json e inclusão de endpoint para pesquisar vagas com base no título

--- src/controller/vaga-controller.ts
+++ src/controller/vaga-controller.ts
@@ -6,6 +6,8 @@ import { vagaRepository } from "../repository/vagaRepository";
 import { Fase } from "../entity/enum/Fase";
 import { userRepository } from "../repository/userRepository";
 import { UserType } from "../types/UserType";
+import { Candidatura } from "../entity/Candidatura";
+import { Like } from "typeorm";
 
 export const createVaga = async (req: Request, res: Response, next: NextFunction) => {
 
@@ -40,21 +42,41 @@ export const createVaga = async (req: Request, res: Response, next: NextFunction
 export const getVagas = async (req: Request, res: Response, next: NextFunction) => {
 
     const vagas: Vaga[] = await vagaRepository.find({
-        relations: ["abertaPor","candidaturas"]
+        relations: ["abertaPor","candidaturas", "candidaturas.usuario"]
     });
 
     const filteredVagas = vagas.map(vaga => {
-        const {abertaPor, ...rest} = vaga;
+        const {abertaPor, candidaturas, ...rest} = vaga;
 
-        const responsavel: UserType = {
+        const aplicacoes = candidaturas.map(item => {
+            return {
+                data: item.data,
+                usuario: {
+                    nome: item.usuario.nome,
+                    email: item.usuario.email
+                }
+            }
+        })
+
+        const responsavel = {
             nome: abertaPor.nome,
-            email: abertaPor.email,
-            tipo: abertaPor.tipo
+            email: abertaPor.email
         };
 
-        return {...rest, responsavel};
+        return {...rest, responsavel, aplicacoes};
     })
 
     res.status(200).json(filteredVagas);
 
+}
+
+export const searchVagas = async (req: Request, res: Response, next: NextFunction) => {
+
+    const { titulo } = req.params;
+
+    const list: Vaga[] = await vagaRepository.find({where: {
+        titulo: Like(`%${titulo}%`)
+    }});
+
+    res.status(200).json(list);
 }
--- src/router/vaga-router.ts
+++ src/router/vaga-router.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-import { createVaga, getVagas } from '../controller/vaga-controller';
+import { createVaga, getVagas, searchVagas } from '../controller/vaga-controller';
 
 export const vagaRouter = express.Router();
 
 //documentar no swagger
 vagaRouter.post('/', createVaga);
 vagaRouter.get('/vagas', getVagas);
+vagaRouter.get('/:titulo', searchVagas);
