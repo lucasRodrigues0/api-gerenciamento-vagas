d26752d9b41e36d0d6111000c92348336078d7a0
Lucas Rodrigues <lucasrodrigues119816@gmail.com>
2024-12-04 14:06:06 -0300

    documentação dos endpoints de autenticação

--- src/router/auth-router.ts
+++ src/router/auth-router.ts
@@ -4,7 +4,5 @@ import { login, logout, register } from '../controller/auth-controller';
 export const authRouter = express.Router();
 
 authRouter.post('/register', register);
-//documentar no swagger
 authRouter.post('/login', login);
-//documentar no swagger
 authRouter.post('/logout', logout);
